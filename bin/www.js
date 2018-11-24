e7a80fc31563457d5755f6d21dff6a0d81911e5d
YOUB AYOUB <sis.ayoub.youb@gmail.com>
2018-11-24 06:51:20 +0000

    cc

--- bin/www.js
+++ bin/www.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 const http = require("http");
 const app = require("../server");
 
-const port = "3000";
+const port = process.env.PORT || 3000;
 app.set("port", port);
 
 const server = http.createServer(app);
