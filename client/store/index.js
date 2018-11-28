cbf48e5b3512e270f7bc5d24c7c3bc06f5b08071
YOUB AYOUB <sis.ayoub.youb@gmail.com>
2018-11-28 01:44:20 +0000

    axios

--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,17 @@
-import { GET_ERRORS, CLEAR_ERRORS } from "../actions/types";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import reducer from "../reducers";
 
 const initialState = {};
 
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case GET_ERRORS:
-      return action.payload;
-    case CLEAR_ERRORS:
-      return {};
-    default:
-      return state;
-  }
-}
+const middleware = applyMiddleware(thunk, logger);
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(middleware)
+);
+
+export default store;
